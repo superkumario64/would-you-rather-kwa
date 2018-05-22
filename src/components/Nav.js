abc8c41d3ce988d94573aa1e4e6d0c3b88f684bb
Kumar Agarwal <superkumario64@gmail.com>
2018-05-22 11:24:40 -0700

    refactor name and logout into nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
+import { handleLogout } from "../actions/shared";
 
-export default function Nav() {
-    return (
-        <nav className='nav'>
-            <ul>
-                <li>
-                    <NavLink to='/' exact activeClassName='active'>
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/add' activeClassName='active'>
-                        Add Question
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    )
+class Nav extends Component {
+    doLogout = () => {
+        this.props.dispatch(handleLogout())
+    }
+    render(){
+        return (
+            <div>
+                <h3>Welcome {this.props.authedUser}</h3>
+                <button
+                    onClick={() => this.doLogout()}
+                >
+                    Logout
+                </button>
+                <nav className='nav'>
+                    <ul>
+                        <li>
+                            <NavLink to='/' exact activeClassName='active'>
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/add' activeClassName='active'>
+                                Add Question
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
 }
+
+function mapStateToProps({ authedUser }) {
+    return {
+        authedUser
+    }
+}
+
+export default connect(mapStateToProps)(Nav)
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import Login from './Login'
 import Question from './Question'
-import { handleLogout } from '../actions/shared'
 
 class QuestionList extends Component {
-    doLogout = () => {
-        this.props.dispatch(handleLogout())
-    }
 
     render() {
         return (
             <div className='question_list'>
-                <h3>Welcome {this.props.authedUser}</h3>
                 <h4>Question List</h4>
-                <button
-                    onClick={() => this.doLogout()}
-                >
-                    Logout
-                </button>
                 <ul>
                     {this.props.questionIds.map((id) => (
                         <li key={id}>
