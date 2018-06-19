3a7a2dea35028dfb9b3babd12cf4cdbf337ccf8f
Kumar Agarwal <superkumario64@gmail.com>
2018-06-19 07:35:55 -0700

    QuestionAnswered page

--- src/components/QuestionAnswered.js
+++ src/components/QuestionAnswered.js
@@ -2,13 +2,52 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class QuestionAnswered extends Component {
+    // function displayAuthedUsersChoice(){
+    //
+    // }
+
+    displayAuthedUsersChoice = (option, authedUser) => {
+        return (option.votes.includes(authedUser)) ?  " (You voted for this option)" : "";
+    }
+
     render() {
+        const { authedUser, question_id, questions } = this.props
+
+        const optionOne = questions[question_id].optionOne;
+        const optionTwo = questions[question_id].optionTwo;
+
+        const optionOneVotes = optionOne.votes.length
+        const optionTwoVotes = optionTwo.votes.length
+
+        const totalVotes = optionOneVotes + optionTwoVotes;
+
+        const optionOnePercent = optionOneVotes/totalVotes*100
+        const optionTwoPercent = optionTwoVotes/totalVotes*100
+
         return (
             <div>
-                QuestionAnswered
+                <h3>Would you Rather...</h3>
+                <h4>optionOne: "{optionOne.text}" got {optionOneVotes} votes which is {optionOnePercent}% of the total votes.
+                    <span className='your-vote'>
+                        {this.displayAuthedUsersChoice(optionOne, authedUser)}
+                    </span>
+                </h4>
+                <h4>optionOne: "{optionTwo.text}" got {optionTwoVotes} votes which is {optionTwoPercent}% of the total votes.
+                    <span className='your-vote'>
+                        {this.displayAuthedUsersChoice(optionTwo, authedUser)}
+                    </span>
+                </h4>
             </div>
         )
     }
 }
 
-export default connect()(QuestionAnswered)
+function mapStateToProps({authedUser, users, questions}) {
+    return {
+        authedUser,
+        users,
+        questions
+    }
+}
+
+export default connect(mapStateToProps)(QuestionAnswered)
